7e0fe360db33d5d82fcc0fef4ecdc068ba7855a6
Eusebio Bortos <ebortos.eb@gmail.com>
2025-07-23 23:12:37 +0200

    Added new processes files

---
+++ Progetto/src/sportello.c
@@ -0,0 +1,11 @@
+#include "../include/shared.h"
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        printf("SPORTELLO TEST (OK?): %s\n", argv[1]);
+    }
+    return 0;
+}
