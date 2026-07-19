5c4e916c4fb8a0d8ffb713bb431ee9457ca39097
ebortos <149820011+ebortos@users.noreply.github.com>
2026-07-19 18:01:28 +0200

    Delete Progetto(old) directory

--- Progetto(old)/include/operatore.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPERATORE_H
-#define OPERATORE_H
-
-void handle_signal(int sig);
-void send_stats(int msg_queue_id);
-int calc_service_time(int base_time);
-void process_ticket(int msg_queue_id, int operator_service);
-
-#endif
